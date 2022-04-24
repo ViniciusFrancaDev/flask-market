1ee465fbe37811e2bb591bb36ec12600136fac85
Vinicius Franca <vfranca14@gmail.com>
2022-04-24 08:24:16 -0300

    Creating market route and sending items

--- market.py
+++ market.py
@@ -6,3 +6,12 @@ app = Flask(__name__)
 @app.route("/home")
 def home_page():
     return render_template("home.html")
+
+@app.route("/market")
+def market_page():
+    items = [
+        {"id": 1, "name": "Phone", "barcode": "893212299897", "price": 500},
+        {"id": 2, "name": "Laptop", "barcode": "123852779847", "price": 900},
+        {"id": 3, "name": "Keyboard", "barcode": "420212887267", "price": 150},
+    ]
+    return render_template("market.html", items=items)
